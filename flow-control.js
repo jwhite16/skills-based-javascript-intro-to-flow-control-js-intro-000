c4846eb1f8c409b21af4927c3a7201e0839e0f49
Joanna White <joanna1016@hotmail.com>
2019-02-26 03:18:08 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,7 +30,8 @@ function switchAge(age) {
   switch (age) {
     case age >= 13 && age <= 19:
     return "You are a teenager";
-    default:
+    break;
+  default:
     return "You have an age";
   }
 };
