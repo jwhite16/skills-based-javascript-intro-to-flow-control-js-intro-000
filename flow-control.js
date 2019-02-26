e0347cd6506d126a9d772655d47b10ab3316d6f1
Joanna White <joanna1016@hotmail.com>
2019-02-26 03:09:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,8 +27,10 @@ function ternaryTeenager(age) {
 };
 
 function switchAge(age) {
-  case age >= 13 && age <= 19:
-  return "You are a teenager";
-  default:
-  return "You have an age";
+  switch (age) {
+    case age >= 13 && age <= 19:
+    return "You are a teenager";
+    default:
+    return "You have an age";
+  }
 };
